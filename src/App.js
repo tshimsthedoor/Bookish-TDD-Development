803477c8a72e0628ca18e42178c63cc1938c66f6
tshimsthedoor <tshims79@gmail.com>
2023-11-07 19:48:37 +0200

    Now our test is passing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
-import "./App.css";
+
 
 
 
 function App() {
   return (
-    <><div>
-      <Typography variant="h2" component="h2" data-test="heading">
-        Bookish
-      </Typography>
-    </div><div data-test='book-list'>
+    <>
+      <div>
+        <Typography variant="h2" component="h2" data-test="heading">
+          Bookish
+        </Typography>
+      </div>
+      <div data-test="book-list">
         <div className="book-item">
-
+          <h2 className="title">Refactoring</h2>
         </div>
         <div className="book-item">
-
+          <h2 className="title">Domain-driven design</h2>
         </div>
-      </div></>
-      
-    
-    
+      </div>
+    </>
   );
 }
 

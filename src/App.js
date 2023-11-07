81a09086e0466d98271369bb8f9bc908e6f4afb6
tshimsthedoor <tshims79@gmail.com>
2023-11-07 20:31:26 +0200

    I refactored the code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
-
-
+import BookList from "./BookList";
 
 
 function App() {
+  const books = [{ name: "Refactoring" }, { name: "Domain-driven design" }];
   return (
-    <>
-      <div>
-        <Typography variant="h2" component="h2" data-test="heading">
-          Bookish
-        </Typography>
-      </div>
-      <div data-test="book-list">
-        <div className="book-item">
-          <h2 className="title">Refactoring</h2>
-        </div>
-        <div className="book-item">
-          <h2 className="title">Domain-driven design</h2>
-        </div>
-      </div>
-    </>
+    
+    
+      <><Typography variant="h2" component="h2" data-test="heading">
+      Bookish
+    </Typography>
+    <BookList books={books} /></>
+    
   );
 }
 

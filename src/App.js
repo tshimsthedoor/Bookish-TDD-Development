920e980342cfaeb59c124119e14790ea9503bcce
tshimsthedoor <tshims79@gmail.com>
2023-11-07 19:20:36 +0200

    make the first e2e test pass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Typography } from "@material-ui/core";
+import "./App.css";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookish
+      </Typography>
     </div>
   );
 }
